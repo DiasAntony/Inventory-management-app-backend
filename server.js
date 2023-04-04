7f43a321958f68c07e7eaed6b4798d8209694c41
diaz <diazantony9997@example.com>
2023-04-04 20:28:38 +0530

    mongoose error

--- server.js
+++ server.js
@@ -43,7 +43,16 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,
+    autoIndex: false, // Don't build indexes
+    maxPoolSize: 10, // Maintain up to 10 socket connections
+    serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+    family: 4, // Use IPv4, skip trying IPv6
+  })
   .then(() => {
     app.listen(PORT, () => {
       console.log(`DB and Server Running on port ${5000}`);
